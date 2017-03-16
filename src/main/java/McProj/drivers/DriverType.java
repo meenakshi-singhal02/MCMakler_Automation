ef6b247ed7d2278df9d49d69eee4f50519e208ed
meenakshi-singhal02 <meenakshi.singhal02@gmail.com>
2017-03-16 09:18:54 +0530

    Have done some changes in browser stack code

--- src/main/java/McProj/drivers/DriverType.java
+++ src/main/java/McProj/drivers/DriverType.java
@@ -1,9 +1,13 @@
 package McProj.drivers;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Enum for generating Specific Driver Type.
@@ -57,6 +61,37 @@ public enum DriverType implements IDriverSetup {
 			return null;
 		}
 	},
+	
+	REMOTE {
+
+		public WebDriver getDriverObject(DesiredCapabilities dc) {
+			// TODO Auto-generated method stub
+			WebDriver driver = null;
+			final String USERNAME = "meenakshisinghal1";
+			  final String AUTOMATE_KEY = "zJHaxsMskytW1xxJ19qH";
+			  final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+			try {
+				 driver = new RemoteWebDriver(new URL(URL), dc);
+				//return new RemoteWebDriver(new URL(URL), dc);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return driver;
+		}
+
+		public DesiredCapabilities getDesiredCapabilities() {
+			// TODO Auto-generated method stub
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability("browser", "IE");
+			capabilities.setCapability("browser_version", "7.0");
+			capabilities.setCapability("os", "Windows");
+			capabilities.setCapability("os_version", "XP");
+			capabilities.setCapability("browserstack.debug", "true");
+			return capabilities;
+		}
+		
+	}
 
 
 
